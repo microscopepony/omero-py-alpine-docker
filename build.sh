4a503a1beb2eb3281f91a63809863c67d1987c40
Simon Li <orpheus+devel@gmail.com>
2018-10-07 14:17:41 +0100

    sed replacement includes /

--- build.sh
+++ build.sh
@@ -12,5 +12,5 @@ IMAGE_PY_TAG="$REPO:${TAG_PREFIX}py"
 IMAGE_WEB_TAG="$REPO:${TAG_PREFIX}web"
 
 docker build -t $IMAGE_PY_TAG .
-sed -i web/Dockerfile -re "s/^FROM\\s+.+/FROM $IMAGE_PY_TAG/"
+sed -i web/Dockerfile -re "s%^FROM\\s+.+%FROM $IMAGE_PY_TAG%"
 docker build -t $IMAGE_WEB_TAG web
