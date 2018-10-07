adda7336af8af38cdbd27490c24de434b5b3b860
Simon Li <orpheus+devel@gmail.com>
2018-10-07 14:52:52 +0100

    Put docker push into a loop

--- build.sh
+++ build.sh
@@ -11,6 +11,7 @@ fi
 IMAGE_PY_TAG="$REPO:${TAG_PREFIX}py"
 IMAGE_WEB_TAG="$REPO:${TAG_PREFIX}web"
 
-docker build -t $IMAGE_PY_TAG .
 sed -i web/Dockerfile -re "s%^FROM\\s+.+%FROM $IMAGE_PY_TAG%"
+
+docker build -t $IMAGE_PY_TAG py
 docker build -t $IMAGE_WEB_TAG web
