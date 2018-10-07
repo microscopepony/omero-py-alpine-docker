e5ee0d5aea56decf9cedc0c974c1f9b7becb45ae
Simon Li <orpheus+devel@gmail.com>
2018-10-07 15:15:08 +0100

    Add omero-web with apps image

--- build.sh
+++ build.sh
@@ -10,8 +10,11 @@ fi
 
 IMAGE_PY_TAG="$REPO:${TAG_PREFIX}py"
 IMAGE_WEB_TAG="$REPO:${TAG_PREFIX}web"
+IMAGE_WEBAPPS_TAG="$REPO:${TAG_PREFIX}webapps"
 
 sed -i web/Dockerfile -re "s%^FROM\\s+.+%FROM $IMAGE_PY_TAG%"
+sed -i webapps/Dockerfile -re "s%^FROM\\s+.+%FROM $IMAGE_WEB_TAG%"
 
 docker build -t $IMAGE_PY_TAG py
 docker build -t $IMAGE_WEB_TAG web
+docker build -t $IMAGE_WEBAPPS_TAG webapps
