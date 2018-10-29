11475f5c9b2082d1f559370648cd860171ce324f
Simon Li <spli@dundee.ac.uk>
2018-10-29 17:34:21 +0000

    Change name to repo/omero-{py,web,webapps}:branch

--- build.sh
+++ build.sh
@@ -3,14 +3,14 @@ set -e
 
 REPO="${CI_REGISTRY_IMAGE:-omero-py-alpine}"
 if [ -n "$CI_COMMIT_REF_SLUG" ]; then
-  TAG_PREFIX="$CI_COMMIT_REF_SLUG-"
+  TAG_PREFIX="$CI_COMMIT_REF_SLUG"
 else
-  TAG_PREFIX=""
+  TAG_PREFIX="latest"
 fi
 
-IMAGE_PY_TAG="$REPO:${TAG_PREFIX}py"
-IMAGE_WEB_TAG="$REPO:${TAG_PREFIX}web"
-IMAGE_WEBAPPS_TAG="$REPO:${TAG_PREFIX}webapps"
+IMAGE_PY_TAG="$REPO/omero-py:${TAG_PREFIX}"
+IMAGE_WEB_TAG="$REPO/omero-web:${TAG_PREFIX}"
+IMAGE_WEBAPPS_TAG="$REPO/omero-webapps:${TAG_PREFIX}"
 
 sed -i web/Dockerfile -re "s%^FROM\\s+.+%FROM $IMAGE_PY_TAG%"
 sed -i webapps/Dockerfile -re "s%^FROM\\s+.+%FROM $IMAGE_WEB_TAG%"
@@ -18,3 +18,9 @@ sed -i webapps/Dockerfile -re "s%^FROM\\s+.+%FROM $IMAGE_WEB_TAG%"
 docker build -t $IMAGE_PY_TAG py
 docker build -t $IMAGE_WEB_TAG web
 docker build -t $IMAGE_WEBAPPS_TAG webapps
+
+if [ -n "$CI_JOB_TOKEN" ]; then
+  for image in "$IMAGE_PY_TAG" "$IMAGE_WEB_TAG" "$IMAGE_WEBAPPS_TAG"; do
+    docker push $image
+  done
+fi
